97c4dd198c7de8f7e6944fc7f3a6041e5ec3739b
Juan Esteban <oliverosduranj@gmail.com>
2025-04-22 17:00:03 -0500

    HU-5 backend

--- backParcial/src/main/java/com/example/backParcial/Models/dao/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backParcial.Models.dao;
-
-import com.example.backParcial.Models.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
